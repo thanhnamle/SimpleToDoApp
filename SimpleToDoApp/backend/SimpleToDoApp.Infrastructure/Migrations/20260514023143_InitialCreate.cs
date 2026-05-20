7c2de4ccfd1667a546f73c8ca9a89c12fdc51266
Selnamle <Selnamle@SELNAMLE1>
2026-05-20 11:05:04 +0700

    Remove legacy MVC project files

--- SimpleToDoApp/backend/SimpleToDoApp.Infrastructure/Migrations/20260514023143_InitialCreate.cs
+++ SimpleToDoApp/backend/SimpleToDoApp.Infrastructure/Migrations/20260514023143_InitialCreate.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace SimpleToDoApp.Migrations
+namespace SimpleToDoApp.Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
--- SimpleToDoApp/backend/SimpleToDoApp.Infrastructure/Migrations/20260514063229_UpdateReminderTodos.cs
+++ SimpleToDoApp/backend/SimpleToDoApp.Infrastructure/Migrations/20260514063229_UpdateReminderTodos.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace SimpleToDoApp.Migrations
+namespace SimpleToDoApp.Infrastructure.Migrations
 {
     /// <inheritdoc />
     public partial class UpdateReminderTodos : Migration
